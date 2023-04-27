ea1830bf4cdfd1954a40720561593f48c9a64537
Анастасия <fblurry71@gmail.com>
2023-04-27 15:58:16 +0300

    удалила лишнее

--- RCode.R
+++
@@ -1,9 +0,0 @@
-install.packages("ggplot2")
-library(ggplot2)
-library(dplyr)
-
-time_exp <- read.csv(file = "DataSet", header = TRUE)
-ggplot(data = time_exp, aes(x = interval, y = LongProb)) + geom_line()
-
-
-
